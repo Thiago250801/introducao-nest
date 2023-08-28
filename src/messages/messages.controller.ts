9e6e080a8d282091059cccc2a4fdb03fb7a9f753
Thiago250801 <thlimabsgim@gmail.com>
2023-08-28 12:39:03 -0400

    adcionando o endPoint /id

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { MessagesService } from './messages.service';
 
 @Controller('messages')
@@ -7,5 +7,11 @@ export class MessagesController {
   @Get()
   findAll() {
     return this.messagesService.findAll();
+  }
+
+  @Get(':id')
+  findById(@Param() params) {
+    // o + no params transforma o id em number
+    return this.messagesService.findById(+params.id);
   }
 }
